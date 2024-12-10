6c7f0211d0eb67d408860255802c81445ecaa12e
Paloma González <palomabgz@gmail.com>
2024-12-10 18:39:02 -0300

    actualización

--- compra.js
+++ compra.js
@@ -1,11 +1,11 @@
-// Recuperar los datos del sessionStorage
+// SessionStorage
 const productos = JSON.parse(localStorage.getItem('productos')) || [];
 const total = localStorage.getItem('total') || 0;
 
 const resumen = document.getElementById("carrito");
 let resumenTexto = "Resumen de la compra:<br><br>";
 
-// Usar un bucle for tradicional para los productos
+// Bucle productos
 for (let i = 0; i < productos.length; i++) {
     const producto = productos[i];
     resumenTexto += `${producto.nombre}: $${producto.precio}<br>`;
@@ -15,7 +15,7 @@ resumenTexto += `<br>Total a pagar: $${total}`;
 resumen.innerHTML = resumenTexto;
 
 
-// Función envío del Formulario
+// Envío form
 function enviarFormulario(event) {
     event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario
 
@@ -26,27 +26,22 @@ function enviarFormulario(event) {
     const telefono = document.getElementById('numero').value.trim();
 
     if (!nombre || !apellido || !email || !telefono) {
-        alert("Por favor, completa todos los campos de contacto.");
-        return; // detiene la ejecucuión de la función hasta que pase el if
+        alert("Por favor, completa todos los campos");
+        return;
     }
 
-    // Crea el listado del carrito
+    // Listado Carrito
     let carritoContenido = '';
     for (let i = 0; i < productos.length; i++) {
         const producto = productos[i];
         carritoContenido += `${producto.nombre} - $${producto.precio}\n`;
     }
 
-    // Agrega al valor total el signo de pesos $
     const totalConPesos = `$${total}`;
 
-    // Asigna los datos a los campos ocultos del formulario
     document.getElementById('carritoData').value = carritoContenido;
     document.getElementById('totalCarrito').value = totalConPesos;
-
-    // Envía el formulario a Formspree
     document.getElementById('formulario').submit();
 }
 
-// Asociamos el evento al botón de enviar
-document.getElementById('botonEnviar').addEventListener('click', enviarFormulario);
+    document.getElementById('botonEnviar').addEventListener('click', enviarFormulario);
